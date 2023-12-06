ca8a82275e38b451c782ec2dcc93bea6e5813de2
edufalcao14 <eduardo.sampaio@student.vinci.be>
2023-12-06 22:49:30 +0100

    Features : all points done and working

---
+++ src/appEntreprise/mainEntreprise.java
@@ -0,0 +1,8 @@
+package appEntreprise;
+
+public class mainEntreprise {
+    public static void main(String[] args) {
+        GestionStageEntreprise gestionStageEntreprise = new GestionStageEntreprise();
+        gestionStageEntreprise.run();
+    }
+}
